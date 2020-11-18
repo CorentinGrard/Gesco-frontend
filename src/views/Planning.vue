af7c8a53d0e9e3f89025b2ccc94b64ca45f4703d
Matthieu Tinnes <matthieu.tinnes@gmail.com>
2020-11-18 17:20:59 +0100

    Amélioration inferface planning 📅

--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -1,32 +1,198 @@
 <template>
-  <v-container fluid>
-    <v-calendar
-      type="week"
-      day-body
-      :weekdays="[1, 2, 3, 4, 5]"
-      :events="sessions"
-      :first-interval= 7
-      :interval-minutes= 60 
-      :interval-count= 13 
-    ></v-calendar>
-  </v-container>
+  <v-row class="fill-height">
+    <v-col>
+      <v-sheet height="64">
+        <v-toolbar
+          flat
+        >
+          <v-btn
+            outlined
+            class="mr-4"
+            color="grey darken-2"
+            @click="setToday"
+          >
+            Aujourd'hui
+          </v-btn>
+          <v-btn
+            fab
+            text
+            small
+            color="grey darken-2"
+            @click="prev"
+          >
+            <v-icon small>
+              mdi-chevron-left
+            </v-icon>
+          </v-btn>
+          <v-btn
+            fab
+            text
+            small
+            color="grey darken-2"
+            @click="next"
+          >
+            <v-icon small>
+              mdi-chevron-right
+            </v-icon>
+          </v-btn>
+          <v-toolbar-title v-if="$refs.calendar">
+            {{ $refs.calendar.title }}
+          </v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-menu
+            bottom
+            right
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                outlined
+                color="grey darken-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <span>{{ typeToLabel[type] }}</span>
+                <v-icon right>
+                  mdi-menu-down
+                </v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="type = 'day'">
+                <v-list-item-title>Jour</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'week'">
+                <v-list-item-title>Semaine</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="type = 'month'">
+                <v-list-item-title>Mois</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar>
+      </v-sheet>
+      <v-sheet height="600">
+        <v-calendar
+          ref="calendar"
+          day-body
+          v-model="focus"
+          color="primary"
+          :events="sessions"
+          :event-color="grey"
+          :type="type"
+          @click:event="showEvent"
+          @click:more="viewDay"
+          @click:date="viewDay"
+          :first-interval= 7
+          :interval-minutes= 60 
+          :interval-count= 13 
+          :weekdays="weekdays"
+        ></v-calendar>
+        <v-menu
+          v-model="selectedOpen"
+          :close-on-content-click="false"
+          :activator="selectedElement"
+          offset-x
+        >
+          <v-card
+            color="grey lighten-4"
+            min-width="350px"
+            flat
+          >
+            <v-toolbar
+              color="grey lighten-4"
+            >
+              <v-btn icon>
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+              <v-spacer></v-spacer>
+              <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+              </v-btn>
+              <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </v-toolbar>
+            <v-card-text>
+              <span v-html="selectedEvent.details"></span>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn
+                text
+                color="secondary"
+                @click="selectedOpen = false"
+              >
+                Cancel
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-menu>
+      </v-sheet>
+    </v-col>
+  </v-row>
 </template>
-
 <script>
 import { mapGetters } from "vuex";
-
-export default {
-  data: () => ({
-    events: [],
+  export default {
+    
+    data: () => ({
+      events: [],
+      focus: '',
+      weekdays:[1, 2, 3, 4, 5],
+      type: 'week',
+      typeToLabel: {
+        month: 'Mois',
+        week: 'Semaine',
+        day: 'Jour',
+      },
+      selectedEvent: {},
+      selectedElement: null,
+      selectedOpen: false,
+    }),
+    computed: mapGetters({
+      sessions: "planning/getSessionForPlanning",
   }),
-  computed: mapGetters({
-    sessions: "planning/getSessionForPlanning",
-  }),
-  created() {
+    created() {
     this.$store.dispatch("planning/getAllSessions");
-  },
-};
-</script>
+    },
+    mounted () {
+      this.$refs.calendar.checkChange()
+    },
+    methods: {
+      viewDay ({ date }) {
+        this.focus = date
+        this.type = 'day'
+      },
+      getEventColor (event) {
+        return event.color
+      },
+      setToday () {
+        this.focus = ''
+      },
+      prev () {
+        this.$refs.calendar.prev()
+      },
+      next () {
+        this.$refs.calendar.next()
+      },
+      showEvent ({ nativeEvent, event }) {
+        const open = () => {
+          this.selectedEvent = event
+          this.selectedElement = nativeEvent.target
+          setTimeout(() => {
+            this.selectedOpen = true
+          }, 10)
+        }
 
-<style>
-</style>
+        if (this.selectedOpen) {
+          this.selectedOpen = false
+          setTimeout(open, 10)
+        } else {
+          open()
+        }
+
+        nativeEvent.stopPropagation()
+      },
+    },
+  }
+</script>
