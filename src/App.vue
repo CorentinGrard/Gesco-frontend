86f74cc59cd9f333a202d2039c606cf63d41b4ba
Corentin Grard <corentin.grard@gmail.com>
2020-11-19 09:40:51 +0100

    new layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,42 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <v-toolbar-title>Gesco</v-toolbar-title>
-      
-      <v-btn text>Planning</v-btn>
-      <v-btn text>Gestion des groupes</v-btn>
-    </v-app-bar>
+    <v-navigation-drawer permanent expand-on-hover app>
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img
+              src="https://randomuser.me/api/portraits/women/85.jpg"
+            ></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title"> Sandra Adams </v-list-item-title>
+            <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list nav dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block> Logout </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
     <v-main>
       <router-view></router-view>
     </v-main>
@@ -17,7 +48,12 @@ export default {
   name: "App",
 
   data: () => ({
-    //
+    items: [
+      { title: "Planning", icon: "mdi-calendar-check" },
+      { title: "Création de cours", icon: "mdi-school" },
+      { title: "Gestion des absences", icon: "mdi-account-off" },
+      { title: "Gestion des notes", icon: "mdi-file-table" },
+    ],
   }),
 };
 </script>
