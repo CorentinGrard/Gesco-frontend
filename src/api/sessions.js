0dc51e7b214933f509a7c8b238749cba4bc0bb98
Matthieu Tinnes <matthieu.tinnes@gmail.com>
2020-12-07 15:15:21 +0100

    add axios instance

--- src/api/sessions.js
+++ src/api/sessions.js
@@ -1,32 +1,11 @@
 /**
  * Mocking client-server processing
  */
-const axios = require('axios');
-const endpoint = "http://127.0.0.1:8000"
-const sessions = [
-  {
-    id: 1,
-    matiere: "maths",
-    type: "cours",
-    detail: "sur teams",
-    obligatoire: true,
-    dateDebut: new Date(Date.parse('2020-11-23T09:00:00')),
-    dateFin: new Date(Date.parse('2020-11-23T12:00:00')),
-  },
-  {
-    id: 2,
-    matiere: "IA",
-    type: "cours",
-    detail: "sur teams",
-    obligatoire: true,
-    dateDebut: new Date(Date.parse('2020-11-23T14:00:00')),
-    dateFin: new Date(Date.parse('2020-11-23T17:00:00')),
-  }
-]
+import axios from '../plugins/axios';
 
 export default {
   getSessions (cb) {
-    return axios.get(endpoint + "/sessions")
+    return axios.backend.get("/sessions")
       .then((response) => cb(response.data))
       .catch((error) => Promise.reject(error))
   },
