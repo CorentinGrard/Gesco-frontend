67ee3ab8f599ae27a71ac30ab5d076c64c7c2b0b
Jeremy <none>
2021-02-22 15:59:00 +0100

    connection aux routes get et add faits, pour que lea y ait acces

--- src/store/modules/admin/formations.js
+++ src/store/modules/admin/formations.js
@@ -1,4 +1,4 @@
-import formation from '../../../api/admin/formation'
+import formationAPI from '../../../api/admin/formation'
 
 // initial state
 const state = () => ({
@@ -12,8 +12,9 @@ const getters = {
         state.data_formations.forEach(formation => {
             formations_list.push({
                 id: formation.id,
-                name: formation.name,
-                responsable: formation.responsable.prenom + ' ' + formation.responsable.nom
+                name: formation.nom,
+                isAlternance: formation.isAlternance,
+                responsable: formation.responsable.Personne.prenom + ' ' + formation.responsable.Personne.nom
             })
         })
         return formations_list
@@ -23,19 +24,37 @@ const getters = {
 // actions
 const actions = {
     initFormations({ commit }) {
-        formation.getData_Formation(formations => {
+        formationAPI.getData_Formation(formations => {
             commit('initFormations', formations)
         })
     },
     addFormation({ commit }, formation) {
-        commit('addFormation', formation)
+        formation['nameFormation'] = formation['name'];
+        delete formation['name'];
+        formation.idResponsable = formation.responsable.id;
+        delete formation['responsable'];
+        formation.isAlternant = true;
+        formationAPI.post_Formation(formation, formation => {
+            commit('addFormation', formation)
+        })
     },
+    editFormation({ commit }, {editedIndex, editedItem}) {
+        formationAPI.put_Formation(editedItem.id, editedItem, semestre => {
+            commit('editFormation', {editedIndex, semestre})
+        })
+    }
+    /*
     removeFormation({ commit }, index) {
         commit('removeFormation', index)
     },
-    editFormation({ commit }, {editedIndex, editedItem}) {
-        commit('editFormation', {editedIndex, editedItem})
-    }
+    removeEleve({ commit }, {editedIndex, editedItem}) {
+        APIeleves.delete_Eleve(
+            editedItem.eleve_id
+        )
+        commit('removeEleve', editedIndex)
+    },
+
+*/
 
 }
 
@@ -45,15 +64,16 @@ const mutations = {
         state.data_formations = formations
     },
     addFormation(state, formation) {
+        console.log(formation);
         state.data_formations.push(formation)
-    },
-    removeFormation(state, index) {
-        state.data_formations.splice(index, 1)
     },
     editFormation(state, {editedIndex, editedItem}){
         Object.assign(state.data_formations[editedIndex], editedItem)
+    },
+    removeFormation(state, index) {
+        console.log("remove: " + index);
+        state.data_formations.splice(index, 1)
     }
-
 }
 
 export default {
